5961e2c95e2132f3874a90fc71f11b1124f57d42
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-01-13 14:33:21 -0300

    Add files via upload

---
+++ pages/Dotacion📊.py
@@ -0,0 +1,82 @@
+import streamlit as st
+
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+
+st.set_page_config(page_title='Dotacion RRHH',
+                    layout='wide',
+                    initial_sidebar_state="expanded")
+
+st.header('Contrataciones')
+
+@st.cache
+def get_data():
+    path =r'Conceptos Noviembre.xlsx'
+    hoja =r'Dota General'
+    return pd.read_excel(path,hoja)
+
+df = get_data()
+
+ausup = df[(df["Modalidad"] == "Autoridades Superiores")]["Modalidad"].count()
+cg = df[(df["Modalidad"] == "Carrera Gerencial")]["Modalidad"].count()
+gab = df[(df["Modalidad"] == "Gabinete")]["Modalidad"].count()
+pp = df[(df["Modalidad"] == "Planta Permanente")]["Modalidad"].count()
+pt = df[(df["Modalidad"] == "Plantas Transitorias")]["Modalidad"].count()
+conls = df[(df["Modalidad"] == "CLS")]["Modalidad"].count()
+at = df[(df["Modalidad"] == "AT")]["Modalidad"].count()
+
+ausup_ta = df[(df["Modalidad"] == "Autoridades Superiores")]["Total Asig"].sum()
+cg_ta = df[(df["Modalidad"] == "Carrera Gerencial")]["Total Asig"].sum()
+gab_ta = df[(df["Modalidad"] == "Gabinete")]["Total Asig"].sum()
+pp_ta = df[(df["Modalidad"] == "Planta Permanente")]["Total Asig"].sum()
+pt_ta = df[(df["Modalidad"] == "Plantas Transitorias")]["Total Asig"].sum()
+conls_ta = df[(df["Modalidad"] == "CLS")]["Total Asig"].sum()
+at_ta = df[(df["Modalidad"] == "AT")]["Total Asig"].sum()
+
+ind1, ind2, ind3 = st.columns(3)
+ind4, ind5, ind6, ind7 = st.columns(4)
+
+
+ind1.metric(label='Autoridades Superiores',
+                value=int(ausup),
+                delta='$' + format(ausup_ta,'0,.2f'),
+                delta_color="normal",)
+ind2.metric(label='Carreras Gerenciales',
+                value=int(cg),
+                delta='$' + format(cg_ta,'0,.2f'),
+                delta_color="normal",)
+ind3.metric(label='Gabinetes',
+                value=int(gab),
+                delta='$' + format(gab_ta,'0,.2f'),
+                delta_color="normal",)
+ind4.metric(label='Plantas Permanentes',
+                value=int(pp),
+                delta='$' + format(pp_ta,'0,.2f'),
+                delta_color="normal",)
+ind5.metric(label='Plantas Transitorias',
+                value=int(pt),
+                delta='$' + format(pt_ta,'0,.2f'),
+                delta_color="normal",)
+ind6.metric(label='CLS',
+                value=int(conls),
+                delta='$' + format(conls_ta,'0,.2f'),
+                delta_color="normal",)
+ind7.metric(label='AT',
+                value=int(at),
+                delta='$' + format(at_ta,'0,.2f'),
+                delta_color='normal',)
+
+st.header('Reparticiones')
+
+
+areas = px.bar(data_frame= df,
+                x='Reparticion General',
+                color='Modalidad',
+                barmode= 'relative',
+                color_discrete_sequence=px.colors.qualitative.Set2,
+                width=1100,
+                height=500)
+st.plotly_chart(areas,theme="streamlit", use_conatiner_width=True)
+
---
+++ pages/Ley Micaela👩.py
@@ -0,0 +1,60 @@
+import streamlit as st
+
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(page_title='Ley Micaela',
+                    layout='wide',
+                    initial_sidebar_state='expanded')
+
+
+@st.cache
+def get_data():
+    path =r'Ley micaela.xlsx'
+    return pd.read_excel(path)
+
+df = get_data()
+colaborador = df[df['Rol']=='Colaborador']
+lider = df[df['Rol']=='Líder']
+
+st.header('Progreso total')
+progreso = px.pie(
+                data_frame=df,
+                names='Status Micaela',
+                color_discrete_sequence=px.colors.qualitative.Set2,
+                height=500,
+                width=800,
+                hole=.4)
+st.plotly_chart(progreso,theme="streamlit", use_conatiner_width='centered')
+
+st.header('Progreso por reparticiones')
+
+opcion = st.radio(
+                "Selecciona el rol:",
+                ('Colaborador', 'Lider'))
+
+grafico_col = px.bar(data_frame = colaborador,
+                x='Sigla',
+                color='Status Micaela',
+                labels={'variable':'Status','value':'Agentes'},
+                barmode= 'relative',
+                height=500,
+                width=950,
+                color_discrete_sequence=px.colors.qualitative.Set2)
+grafico_col.update_traces(textfont_size=15, textangle=0, textposition="inside", cliponaxis=True)
+
+grafico_lid = px.bar(
+                    data_frame= lider,
+                    x='Sigla',
+                    color='Status Micaela',
+                    labels={'variable':'Status','value':'Agentes'},
+                    barmode= 'relative',
+                    height=500,
+                    width=950,
+                    color_discrete_sequence=px.colors.qualitative.Set2)
+grafico_lid.update_traces(textfont_size=15, textangle=0, textposition="inside", cliponaxis=True)
+
+if opcion == 'Colaborador':
+    st.plotly_chart(grafico_col, theme="streamlit", use_conatiner_width=True)
+else:
+    st.plotly_chart(grafico_lid, theme="streamlit", use_conatiner_width=True)
