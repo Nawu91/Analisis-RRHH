47c5935b8da2aeadcb9136fb06fec0012c376413
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-01-13 14:45:01 -0300

    Update Dotacion📊.py

--- pages/Dotacion📊.py
+++ pages/Dotacion📊.py
@@ -13,7 +13,7 @@ st.header('Contrataciones')
 
 @st.cache
 def get_data():
-    path =r'Conceptos Noviembre.xlsx'
+    path ='https://github.com/Nawu91/Analisis-RRHH/blob/main/Conceptos%20Noviembre.xlsx'
     hoja =r'Dota General'
     return pd.read_excel(path,hoja)
 
