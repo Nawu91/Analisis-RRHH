779f5326c306be534b0cb704eabfd49180def665
Nawu91 <nawu.sanchez@gmail.com>
2023-01-18 15:13:50 -0300

    Update Dotacion📊.py

--- pages/Dotacion📊.py
+++ pages/Dotacion📊.py
@@ -69,12 +69,12 @@ ind7.metric(label='AT',
                 delta_color='normal',)
 
 st.header('Reparticiones')
-st.multiselect(label='Reparticiones',
+opcion_repa = st.multiselect(label='Reparticiones',
                options=['DGDIYA','SSFPFC','DGAII','DGMUJ','SECISYAI','DGPSA','DGABCO','MDHYHGC','DGPOLA','DGEPS','DGDEIS','DGDIM','DGTALMDHYHGC','DGINFS','COPIDIS','SSDPH','DGAPP','SSADMINIS','UAIMDHYH','UUBPCM','DGINS','DGMV','DGINE','DGOPDU','DGPLCG','FCE']
                )
 
 areas = px.bar(data_frame= df,
-                x='Reparticion General',
+                x=df[df['Rparticion General']==opcion_repa],
                 color='Modalidad',
                 barmode= 'relative',
                 color_discrete_sequence=px.colors.qualitative.Set2,
