90508a1ab9692d9c36ec8b58bf5dbd344ab8e7cc
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-02-17 13:02:54 -0300

    Add files via upload

---
+++ pages/DGPSA.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(page_title='Prueba DGPSA',
+                    layout='wide',
+                    initial_sidebar_state="expanded")
+
+
+components.html(https://app.powerbi.com/view?r=eyJrIjoiYTE5MThiZjctYjIxZC00OWQzLWI3ODAtNzA3OGYwM2EyMmMwIiwidCI6IjIzNzc0NzJlLTgwMDQtNDY0OC04NDU2LWJkOTY4N2FmYTE1MCIsImMiOjR9)
