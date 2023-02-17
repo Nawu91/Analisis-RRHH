ac338ba5f9be33dd20f1eaeafd50fe30ccb8613e
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-02-17 13:43:23 -0300

    Update DGPSA.py

--- pages/DGPSA.py
+++ pages/DGPSA.py
@@ -6,4 +6,4 @@ st.set_page_config(page_title='Prueba DGPSA',
                     initial_sidebar_state="expanded")
 
 
-components.html(https://app.powerbi.com/view?r=eyJrIjoiYTE5MThiZjctYjIxZC00OWQzLWI3ODAtNzA3OGYwM2EyMmMwIiwidCI6IjIzNzc0NzJlLTgwMDQtNDY0OC04NDU2LWJkOTY4N2FmYTE1MCIsImMiOjR9)
+components.html("https://app.powerbi.com/view?r=eyJrIjoiYTE5MThiZjctYjIxZC00OWQzLWI3ODAtNzA3OGYwM2EyMmMwIiwidCI6IjIzNzc0NzJlLTgwMDQtNDY0OC04NDU2LWJkOTY4N2FmYTE1MCIsImMiOjR9")
