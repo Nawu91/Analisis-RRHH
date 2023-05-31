67adaf8458e79f0c56753788b9f837b9c8f41ab2
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-05-31 14:36:08 -0300

    Delete DGPSA.py

--- pages/DGPSA.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import streamlit.components.v1 as components
-
-st.set_page_config(page_title='Prueba DGPSA',
-                    layout='wide',
-                    initial_sidebar_state="expanded")
-
-
-components.iframe("https://app.powerbi.com/view?r=eyJrIjoiYTE5MThiZjctYjIxZC00OWQzLWI3ODAtNzA3OGYwM2EyMmMwIiwidCI6IjIzNzc0NzJlLTgwMDQtNDY0OC04NDU2LWJkOTY4N2FmYTE1MCIsImMiOjR9",width=1200, height=1600)
