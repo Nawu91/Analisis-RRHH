354c77bc1e6145a8a385b944a762d47363034305
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-06-07 12:20:37 -0300

    1

--- pages/02_👩Ley Micaela.py
+++ pages/02_👩Ley Micaela.py
@@ -17,43 +17,46 @@ df = get_data()
 colaborador = df[df['Rol']=='Colaborador']
 lider = df[df['Rol']=='Líder']
 
-progreso= st.columns(1)
+col1, col2 = st.columns(2)
 
-st.header('Progreso total')
-progreso = px.pie(
-                data_frame=df,
-                names='Status Micaela',
-                color_discrete_sequence=px.colors.qualitative.Set2,
-                height=500,
-                width=1200,
-                hole=.4)
-st.plotly_chart(progreso,theme="streamlit", use_conatiner_width='centered')
+with col1:
 
-st.header('Progreso por reparticiones')
+    st.header('Progreso total')
+    progreso = px.pie(
+                    data_frame=df,
+                    names='Status Micaela',
+                    color_discrete_sequence=px.colors.qualitative.Set2,
+                    height=500,
+                    width=1200,
+                    hole=.4)
+    st.plotly_chart(progreso,theme="streamlit", use_conatiner_width='centered')
 
-opcion = st.radio(
-                "Selecciona el rol:",
-                ('Colaborador', 'Lider'))
+    st.header('Progreso por reparticiones')
+with col2:
 
-grafico_col = px.bar(data_frame = colaborador,
-                x='Sigla',
-                color='Status Micaela',
-                labels={'variable':'Status','value':'Agentes'},
-                barmode= 'relative',
-                color_discrete_sequence=px.colors.qualitative.Set2)
-grafico_col.update_traces(textfont_size=15, textangle=0, textposition="inside", cliponaxis=True)
+    opcion = st.radio(
+                    "Selecciona el rol:",
+                    ('Colaborador', 'Lider'))
 
-grafico_lid = px.bar(
-                    data_frame= lider,
+    grafico_col = px.bar(data_frame = colaborador,
                     x='Sigla',
                     color='Status Micaela',
                     labels={'variable':'Status','value':'Agentes'},
                     barmode= 'relative',
                     color_discrete_sequence=px.colors.qualitative.Set2)
-grafico_lid.update_traces(textfont_size=15, textangle=0, textposition="inside", cliponaxis=True)
+    grafico_col.update_traces(textfont_size=15, textangle=0, textposition="inside", cliponaxis=True)
 
-if opcion == 'Colaborador':
-    st.plotly_chart(grafico_col, theme="streamlit",use_conatiner_width='centered')
-else:
-    st.plotly_chart(grafico_lid, theme="streamlit", use_conatiner_width='centered')
+    grafico_lid = px.bar(
+                        data_frame= lider,
+                        x='Sigla',
+                        color='Status Micaela',
+                        labels={'variable':'Status','value':'Agentes'},
+                        barmode= 'relative',
+                        color_discrete_sequence=px.colors.qualitative.Set2)
+    grafico_lid.update_traces(textfont_size=15, textangle=0, textposition="inside", cliponaxis=True)
+
+    if opcion == 'Colaborador':
+        st.plotly_chart(grafico_col, theme="streamlit",use_conatiner_width='centered')
+    else:
+        st.plotly_chart(grafico_lid, theme="streamlit", use_conatiner_width='centered')
 
