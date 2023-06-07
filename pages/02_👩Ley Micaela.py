7befedabaa0200d52d4b6c00f995928d74d5c6d3
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-06-07 12:03:57 -0300

    Update 02_👩Ley Micaela.py

--- pages/02_👩Ley Micaela.py
+++ pages/02_👩Ley Micaela.py
@@ -8,21 +8,3 @@ st.set_page_config(page_title='Ley Micaela',
                     initial_sidebar_state='expanded')
 
 
-@st.cache
-def get_data():
-    path =r'Ley micaela.xlsx'
-    return pd.read_excel(path)
-
-df = get_data()
-colaborador = df[df['Rol']=='Colaborador']
-lider = df[df['Rol']=='Líder']
-
-st.header('Progreso total')
-progreso = px.pie(
-                data_frame=df,
-                names='Status Micaela',
-                color_discrete_sequence=px.colors.qualitative.Set2,
-                height=500,
-                width=600,
-                hole=.4)
-st.plotly_chart(progreso,theme="streamlit", use_conatiner_width=True)
