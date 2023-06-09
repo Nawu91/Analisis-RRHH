15863a694fbd9caabab3a038459f36ac7e303596
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-06-09 14:45:12 -0300

    Update 01_📊Dotacion.py

--- pages/01_📊Dotacion.py
+++ pages/01_📊Dotacion.py
@@ -104,6 +104,6 @@ areas = px.bar(data_frame= df_graf,
                 width=1450,
                 height=500,
                 text_auto=True)
-areas.update_traces(textposition='outside',xaxis={'categoryorder':'category ascending'})
+areas.update_traces(textposition='outside')
 st.plotly_chart(areas,theme="streamlit", use_conatiner_width=True)
 
