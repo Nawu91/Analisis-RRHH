bf0e3545e85813859ac3ce40669c482d4721eb78
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-06-09 11:00:53 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,13 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
+    page_title="Analisis RRHH",
     page_icon='',
     layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
-)
+    initial_sidebar_state="expanded")
