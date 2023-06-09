7f6cced62d885091393746ffd5b0e8557c368054
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-06-09 09:42:28 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,14 @@
 import streamlit as st
+from streamlit_themes import st_theme
 
 
 
 st.set_page_config(page_title='Dotacion RRHH',
                     layout='wide',
                     initial_sidebar_state='expanded')
+
+selected_theme = st.sidebar.selectbox(
+    "Select Theme",
+    ("Default", "Darkly", "Lightly", "Cheerful", "Dark", "Material")
+)
+st_theme(selected_theme)
