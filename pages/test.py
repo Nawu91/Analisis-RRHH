4f6ab9fdcd03fc1bf4c4003650697c34d3e927f8
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-06-16 14:16:21 -0300

    1

--- pages/test.py
+++ pages/test.py
@@ -17,12 +17,13 @@ periodos = df_anual['Periodo']
 lista_periodo = periodos.unique().tolist()
 selected_periodo = st.selectbox('Selecciona el periodo', lista_periodo)
 df= df_anual[df_anual['Periodo'] == selected_periodo]
-
+df_count = df_anual.groupby(['Periodo', 'Modalidad']).size().reset_index(name='Count')
 
 st.header('Anual 2023')
 
-lineas = px.bar(df_anual, 
-                x="Periodo", 
+lineas = px.bar(df_count, 
+                x="Periodo",
+                y='Count' 
                 color='Modalidad',
                 barmode='group',
                 width=1450,
