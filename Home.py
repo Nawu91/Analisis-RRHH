0ed25c2d00570a5b0e97d3639c0eac6bfa87b63d
Nawu91 <nawu.sanchez@gmail.com>
2023-06-23 13:57:59 -0300

    1

--- Home.py
+++ Home.py
@@ -7,7 +7,6 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded")
 
-
 background = '''
     <style>
     body {
@@ -19,6 +18,8 @@ background = '''
 
 st.markdown(background, unsafe_allow_html=True)
 
+
+sidebar_container = st.sidebar.container()
 
 with sidebar_container:
     st.image('bavos-footer.png', caption='DGDIM-RRHH', use_column_width=True)
