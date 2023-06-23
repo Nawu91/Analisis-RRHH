158a2607cf9d8480c6246702707cd9bce4824668
Nawu91 <nawu.sanchez@gmail.com>
2023-06-23 14:59:47 -0300

    1

--- Home.py
+++ Home.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import streamlit.components.v1 as components
+from streamlit_extras.app_logo import add_logo
+
+add_logo("BA.jpg")
 
 sidebar_container = st.sidebar.container()
 with sidebar_container:
     st.image('bavos-footer.png', caption='DGDIM-RRHH', use_column_width=True)
     
 st.title('Gerencia Operativa Gestión de Recursos Humanos')
-
-
-
