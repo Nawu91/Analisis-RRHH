694e4da69c7d353220e5103dccfd6a2bf069f5fd
Nawu91 <nawu.sanchez@gmail.com>
2023-06-23 14:18:17 -0300

    1

--- Home.py
+++ Home.py
@@ -1,17 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-background = '''
-    <style>
-    body {
-        background-image: url("BA.jpg");
-        color: red; /* Mensaje de prueba para verificar si el CSS se aplica correctamente */
-    }
-    </style>
-'''
-st.markdown(background, unsafe_allow_html=True)
-
-
 sidebar_container = st.sidebar.container()
 with sidebar_container:
     st.image('bavos-footer.png', caption='DGDIM-RRHH', use_column_width=True)
