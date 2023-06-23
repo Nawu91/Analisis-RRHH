06f74f295f98a9a8f17905889f207b33b69dc6ac
Nawu91 <nawu.sanchez@gmail.com>
2023-06-23 13:51:49 -0300

    1

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.set_page_config(
     page_title="Analisis RRHH",
@@ -7,7 +8,19 @@ st.set_page_config(
     initial_sidebar_state="expanded")
 
 sidebar_container = st.sidebar.container()
+background = '''
+    <style>
+    body {
+        background-image: url("nombre_de_la_imagen.jpg");
+        background-size: cover;
+    }
+    </style>
+'''
+
+st.markdown(background, unsafe_allow_html=True)
+
+
 with sidebar_container:
     st.image('bavos-footer.png', caption='DGDIM-RRHH', use_column_width=True)
 
-st.markdown('<style src="styles.css"></style>', unsafe_allow_html=True)
+
