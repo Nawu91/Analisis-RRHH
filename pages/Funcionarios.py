69798b9b4397a8e9144ed3b2057f937b361f1047
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-06-28 12:41:21 -0300

    1

--- pages/Funcionarios.py
+++ pages/Funcionarios.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import os
+import pandas as pd
+import numpy as np
 
+@st.cache_data
+def get_data():
+    path =r'Funcionarios y UR MDHYH 2021.xlsx'
+    return pd.read_excel(path)
+df = get_data()
