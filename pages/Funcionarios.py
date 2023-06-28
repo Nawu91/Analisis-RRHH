3583e8712f7806adb4fcb43a7d99c45ecc35a8f4
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-06-28 12:43:24 -0300

    1

--- pages/Funcionarios.py
+++ pages/Funcionarios.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 @st.cache_data
-def get_db():
-    path =r'Funcionarios y UR MDHYH 2021.xlsx'
+def get_data():
+    path =r'acumulado.xlsx'
     return pd.read_excel(path)
 df = get_db()
