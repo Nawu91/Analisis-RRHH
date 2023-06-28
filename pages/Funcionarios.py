6444080e30fbc018d9ab7243127f1e11f8326bb8
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-06-28 12:37:20 -0300

    1

--- pages/Funcionarios.py
+++ pages/Funcionarios.py
@@ -4,7 +4,7 @@ import pandas as pd
 @st.cache_data
 def get_data():
     path =r'Funcionarios y UR MDHYH 2021.xlsx'
-    return pd.read_excel(path,sheet_name ='Funcionarios MDHYH')
+    return pd.read_excel(path)
 df = get_data()
 
 df_editable = st.data_editor(df,num_rows='dynamic')
