bc9f2f9dcda91d5a1b73649f4bbe8f4f4036476a
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-06-28 15:25:42 -0300

    1

--- pages/Funcionarios.py
+++ pages/Funcionarios.py
@@ -6,8 +6,8 @@ import numpy as np
 @st.cache_data
 
 def get_db():
-    path =r'acumulado.xlsx'
-    return pd.read_excel(path)
+    path =r'Funcionarios y UR MDHYH 2021.xlsx'
+    return pd.read_excel(path,sheet_name='Funcionarios MDHYH')
 df = get_db()
 
 df_edit = st.data_editor(df, num_rows="dynamic")
