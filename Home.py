5644b1ae816268918972bfcc1c63df58285149fc
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-07-05 20:04:10 -0300

    1

--- Home.py
+++ Home.py
@@ -1,7 +1,9 @@
 import streamlit as st
-import streamlit.components.v1 as components
 from streamlit_extras.app_logo import add_logo
 
+st.set_page_config(page_title='App RRHH',
+                    layout='wide',
+                    initial_sidebar_state="auto")
 add_logo("bavos-footer.png")
    
 st.title('Gerencia Operativa Gestión de Recursos Humanos')
