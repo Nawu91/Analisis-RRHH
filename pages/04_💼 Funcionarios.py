5d55ccee0f5d0ad83a58a0a6e69f741a6b7a8b69
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-07-05 15:20:42 -0300

    1

--- pages/04_💼 Funcionarios.py
+++ pages/04_💼 Funcionarios.py
@@ -7,6 +7,8 @@ st.set_page_config(page_title='Funcionarios',
                     initial_sidebar_state="auto")
 add_logo("bavos-footer.png")
 
+st.title("Organigrama MDHYHGC")
+
 with open("org.html", "r") as file:
     html_code = file.read()
 components.html(html_code, width=1100, height=1800, scrolling=True)
