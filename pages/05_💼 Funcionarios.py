03923c592526300cff561b41d735efe64b363de5
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-07-05 15:15:38 -0300

    1

--- pages/05_💼 Funcionarios.py
+++ pages/05_💼 Funcionarios.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+st.set_page_config(page_title='Funcionarios',
+                    layout='wide',
+                    initial_sidebar_state="auto")
+add_logo("bavos-footer.png")
+
 with open("org.html", "r") as file:
     html_code = file.read()
 components.html(html_code, width=1100, height=1800, scrolling=True)
