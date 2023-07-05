a248d9ee5e254afacafd1604cb50351e2e53aa5b
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-07-05 14:33:09 -0300

    1

--- pages/05_Funcionarios.py
+++ pages/05_Funcionarios.py
@@ -2,6 +2,6 @@ import streamlit as st
 from streamlit import components
 
 def main():
-    with open("org.html", "r") as file:
+    with open("org.html") as file:
         html_code = file.read()
     components.html(html_code, width=800, height=600, scrolling=True)
