3719cee819c466bb86969412955d8b4eb0a0e9a8
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-07-07 13:06:31 -0300

    1

--- pages/04_💼 Funcionarios.py
+++ pages/04_💼 Funcionarios.py
@@ -11,4 +11,4 @@ st.title('Organigrama MDHYHGC')
 
 with open("org.html", "r") as file:
     html_code = file.read()
-components.html(html_code, width=1100, height=800, scrolling=True)
+components.html(html_code, width=1100, height=1500, scrolling=True)
