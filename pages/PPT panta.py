2fa55d1796a49a68514969b1143b9e0b7ad79546
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-07-28 15:28:06 -0300

    1

--- pages/PPT panta.py
+++ pages/PPT panta.py
@@ -21,11 +21,10 @@ with st.expander("diagrama 1"):
 with st.expander("diagrama 2"):
    components.html(html_code, width=1700, height=800, scrolling=True)
 
-with st.expander("-"):
-  st.image("PPT GORRHH 1.jpg",width= 1700)
+st.image("PPT GORRHH 1.jpg",width= 1700)
 
-with st.expander("-"):
-  st.image("PPT GORRHH 2.jpg",width= 1700)
+
+st.image("PPT GORRHH 2.jpg",width= 1700)
 
 with st.expander("graf 1"):
   st.pyplot()
@@ -33,5 +32,4 @@ with st.expander("graf 1"):
 with st.expander("graf 2"):
   st.pyplot()
 
-with st.expander("-"):
-  st.image("PPT GORRHH 3.jpg",width= 1700)
+st.image("PPT GORRHH 3.jpg",width= 1700)
