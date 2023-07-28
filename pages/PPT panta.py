9de651765fa2cec66dbede452f60f2af104f9904
Nawu91 <122503233+Nawu91@users.noreply.github.com>
2023-07-28 14:57:14 -0300

    Update PPT panta.py

--- pages/PPT panta.py
+++ pages/PPT panta.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import streamlit.components.v1 as components
+from streamlit_extras.app_logo import add_logo
 
+st.set_page_config(page_title='PPT',
+                    layout='wide',
+                    initial_sidebar_state="auto")
+add_logo("bavos-footer.png")
+
+st.title('Presentacion RRHH')
